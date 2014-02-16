91b1b9bbc3a2a5b514c06be6e9887f6210df1d98
Jeckel <jeckel@jeckel.fr>
2014-02-16 20:00:09 +0100

    Add the 'wave' effect

--- ArduinoMain/src/sketch.ino
+++ ArduinoMain/src/sketch.ino
@@ -20,7 +20,7 @@ void setup() {
  *  Main loop
  */
 void loop() {
-	for(int i = 0; i < 3; i++) {
+	/*for(int i = 0; i < 3; i++) {
 		for(int iLed = 0; iLed < NUM_LEDS; iLed++) {
 			switch(i) {
 			 	case 0: leds[iLed] = 0xff0000; break; // red
@@ -30,5 +30,53 @@ void loop() {
 			LED.showRGB((byte*)leds, NUM_LEDS);
 			memset(leds, 0x00, NUM_LEDS * sizeof(CRGB));
 		}
-	}
+	} */
+	wave();
 }
+
+
+void wave()
+{
+    byte r, g, b, y;
+    byte cycle = 8;
+    CRGB color;
+    color = 0xff0000;
+
+    for(int j = 0; j < (cycle * 4); j++)
+    {
+        for(int iLed = 0; iLed < NUM_LEDS; iLed++)
+        {
+            y = (iLed + j) & 0x1f;
+            if (y >= (2 * cycle))
+            {
+                if (y >= (3 * cycle))
+                {
+                    r = 255 * ((4 * cycle) - y) / cycle;
+                    g = 255 * ((4 * cycle) - y) / cycle;
+                    b = 255 * ((4 * cycle) - y) / cycle;
+                }
+                else
+                {
+                    r = 255 * (y - (2 * cycle)) / cycle;
+                    g = 255 * (y - (2 * cycle)) / cycle;
+                    b = 255 * (y - (2 * cycle)) / cycle;
+                }
+            } else {
+                if (y >= cycle)
+                {
+                    r = color.r * ((2 * cycle) - y) / cycle;
+                    g = color.g * ((2 * cycle) - y) / cycle;
+                    b = color.b * ((2 * cycle) - y) / cycle;
+                }
+                else
+                {
+                    r = color.r * y / cycle;
+                    g = color.g * y / cycle;
+                    b = color.b * y / cycle;
+                }
+            }
+            leds[iLed] = (CRGB) {b, r, g};
+        }
+        LED.showRGB((byte*)leds, NUM_LEDS);
+    }
+}    // function wave
