d964e3fbbc910a924148cb7dc8182051f4e828da
Jeckel <jeckel@jeckel.fr>
2014-02-16 16:27:53 +0100

    Test script for FastSPI

--- ArduinoMain/src/sketch.ino
+++ ArduinoMain/src/sketch.ino
@@ -1,15 +1,27 @@
+#include "FastSPI_LED2.h"
 
-#define LED_PIN 13
+#define NUM_LEDS 150
+struct CRGB { byte g; byte r; byte b; };
+struct CRGB leds[NUM_LEDS];
+TM1809Controller800Mhz<6> LED;
 
-void setup()
-{
-    pinMode(LED_PIN, OUTPUT);
+void setup() {
+	LED.init();
+	memset(leds, 255, NUM_LEDS * sizeof(struct CRGB));
+	LED.showRGB((byte*)leds, NUM_LEDS);
+	delay(20);
 }
 
-void loop()
-{
-    digitalWrite(LED_PIN, HIGH);
-    delay(100);
-    digitalWrite(LED_PIN, LOW);
-    delay(900);
+void loop() {
+	for(int i = 0; i < 3; i++) {
+		for(int iLed = 0; iLed < NUM_LEDS; iLed++) {
+			memset(leds, 0,  NUM_LEDS * sizeof(struct CRGB));
+			switch(i) {
+			 	case 0: leds[iLed].r = 128; break;
+			 	case 1: leds[iLed].g = 128; break;
+			 	case 2: leds[iLed].b = 128; break;
+			 }
+			LED.showRGB((byte*)leds, NUM_LEDS);;
+		}
+	}
 }
