37207ad95ceeb55b9cd4fbe1741912a4d7c66a57
Jeckel <jeckel@jeckel.fr>
2014-02-16 23:50:47 +0100

    Convert Wave method into WaveEffect class

--- ArduinoMain/src/Effect/Effect.h
+++ ArduinoMain/src/Effect/Effect.h
@@ -32,4 +32,61 @@ class ColorChaseEffect : public Effect {
 };
 
 
+class WaveEffect : public Effect {
+	public:
+		WaveEffect(CRGB *leds, CRGB color) : Effect(leds), color(color) {}
+		void step() {
+			byte y;
+			byte cycle = 8;
+			for(int iLed = 0; iLed < NUM_LEDS; iLed++)
+			{
+				// y is evolving from 0 to (4 x Cycle)
+				y = (iLed + step_index) & 0x1f;
+				if (y >= (2 * cycle))
+				{
+					if (y >= (3 * cycle)) // y in 4th cycle
+					{
+						leds[iLed] = (CRGB) {
+							0xff >> (y - 24),
+							0xff >> (y - 24),
+							0xff >> (y - 24)
+						};
+					}
+					else				// y in 3rd cycle
+					{
+						leds[iLed] = (CRGB) {
+							0xff >> (24 - y),
+							0xff >> (24 - y),
+							0xff >> (24 - y)
+						};
+					}
+				} else {
+					if (y >= cycle)		// y in 2nd cycle
+					{
+						leds[iLed] = (CRGB) {
+							color.b >> (y - 8),
+							color.r >> (y - 8),
+							color.g >> (y - 8),
+						};
+					}
+					else				// y in 1st cycle
+					{
+						leds[iLed] = (CRGB) {
+							color.b >> (8 - y),
+							color.r >> (8 - y),
+							color.g >> (8 - y),
+						};
+					}
+				}
+			}
+			step_index ++;
+			if (step_index >= 32) {
+				step_index = 0;
+			}
+		}
+	private :
+		CRGB color;
+};
+
+
 #endif
--- ArduinoMain/src/sketch.ino
+++ ArduinoMain/src/sketch.ino
@@ -21,79 +21,13 @@ void setup() {
  *  Main loop
  */
 void loop() {
-	/*for(int i = 0; i < 3; i++) {
-		for(int iLed = 0; iLed < NUM_LEDS; iLed++) {
-			switch(i) {
-			 	case 0: leds[iLed] = 0xff0000; break; // red
-			 	case 1: leds[iLed] = 0x00ff00; break; // green
-			 	case 2: leds[iLed] = 0x0000ff; break; // blue
-			}
-			LED.showRGB((byte*)leds, NUM_LEDS);
-			memset(leds, 0x00, NUM_LEDS * sizeof(CRGB));
-		}
-	} */
-	//wave();
-	CRGB Red = {0xff, 0xff, 0x00};
-	ColorChaseEffect effect = ColorChaseEffect(leds, Red);
+	CRGB Red;
+	Red = 0xff0000;
+	//ColorChaseEffect effect = ColorChaseEffect(leds, Red);
+	WaveEffect effect = WaveEffect(leds, Red);
 	while(true) {
 		effect.step();
 		LED.showRGB((byte*)leds, NUM_LEDS);
 		effect.after_step();
 	}
 }
-
-
-void wave()
-{
-    byte r, g, b, y;
-    byte cycle = 8;
-    CRGB color;
-    color = 0xff0000;
-
-    for(int j = 0; j < (cycle * 4); j++)
-    {
-        for(int iLed = 0; iLed < NUM_LEDS; iLed++)
-        {
-        	// y is evolving from 0 to (4 x Cycle)
-            y = (iLed + j) & 0x1f;
-            if (y >= (2 * cycle))
-            {
-                if (y >= (3 * cycle)) // y in 4th cycle
-                {
-                    leds[iLed] = (CRGB) {
-                    	0xff >> (y - 24),
-                    	0xff >> (y - 24),
-                    	0xff >> (y - 24)
-                    };
-                }
-                else				// y in 3rd cycle
-                {
-                    leds[iLed] = (CRGB) {
-                    	0xff >> (24 - y),
-                    	0xff >> (24 - y),
-                    	0xff >> (24 - y)
-                    };
-                }
-            } else {
-                if (y >= cycle)		// y in 2nd cycle
-                {
-                    leds[iLed] = (CRGB) {
-                    	color.b >> (y - 8),
-                      	color.r >> (y - 8),
-                       	color.g >> (y - 8),
-                    };
-                }
-                else				// y in 1st cycle
-                {
-                    leds[iLed] = (CRGB) {
-                    	color.b >> (8 - y),
-                      	color.r >> (8 - y),
-                       	color.g >> (8 - y),
-                    };
-                }
-            }
-        }
-        //delay(100);
-        LED.showRGB((byte*)leds, NUM_LEDS);
-    }
-}    // function wave
