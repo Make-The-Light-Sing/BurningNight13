c67385d6147acbbc0dd0886522554f82cbcd01f1
Jeckel <jeckel@jeckel.fr>
2014-03-31 08:43:06 +0200

    Refactoring sensor reading

--- python/Sensor.py
+++ python/Sensor.py
@@ -8,19 +8,37 @@ addr_sensor      = 0x10
 addr_strip_left  = 0x20
 addr_strip_right = 0x21
 
-bus.write_byte(addr_sensor, 1)
-time.sleep(0.001)
-print "Dist 1 : ", bus.read_byte(addr_sensor)
 
-time.sleep(0.001)
-bus.write_byte(addr_sensor, 2)
-time.sleep(0.001)
-print "Dist 2 : ", bus.read_byte(addr_sensor)
+""" Read values from sensor, this values are retrieved from the Arduino via the i2c bus"""
+def readSensorValues():
+    """ Read sensor 1"""
+    bus.write_byte(addr_sensor, 1)
+    time.sleep(0.001)
+    dist_1 = bus.read_byte(addr_sensor)
+    
+    """ Read sensor 2"""
+    time.sleep(0.001)
+    bus.write_byte(addr_sensor, 2)
+    time.sleep(0.001)
+    dist_2 = bus.read_byte(addr_sensor)
+    
+    """ Read sensor 3"""
+    time.sleep(0.001)
+    bus.write_byte(addr_sensor, 3)
+    time.sleep(0.001)
+    dist_3 = bus.read_byte(addr_sensor)
+    
+    """ Return read values"""
+    return [dist_1, dist_2, dist_3]
 
-time.sleep(0.001)
-bus.write_byte(addr_sensor, 3)
-time.sleep(0.001)
-print "Dist 3 : ", bus.read_byte(addr_sensor)
+
+sensorValues = readSensorValues()
+
+print "Dist 1 : ", sensorValues[0]
+print "Dist 2 : ", sensorValues[1]
+print "Dist 3 : ", sensorValues[2]
+
+#bus.write_byte(addr_strip_left, dist_1)
 
 #print "Dist 2 : ", bus.read_byte(address)
 #print "Dist 3 : ", bus.read_byte(address)
