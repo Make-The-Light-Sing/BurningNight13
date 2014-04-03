2ab81d5bb4b0bc53bdeea33369a3c7e4c4edea62
Jeckel <jeckel@jeckel.fr>
2014-04-03 21:38:11 +0200

    Setup example

---
+++ StripLeft/src/Config.h
@@ -0,0 +1,9 @@
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#define SLAVE_ADDRESS 0x20
+#define NUM_LEDS      180
+#define PIN_STRIP     6
+
+
+#endif
--- StripLeft/src/sketch.ino
+++ StripLeft/src/sketch.ino
@@ -1,9 +1,33 @@
 #include <Color.h>
+#include <FastSPI_LED2.h>
 
+#include "Config.h"
+
+CRGB leds[NUM_LEDS];
+TM1809Controller800Mhz<PIN_STRIP> LED;
+
+/**
+ * Initialize
+ */
 void setup()
 {
-}
+    LED.init();
+}   // setup
 
+/**
+ * Main loop
+ */
 void loop()
 {
-}
+    for(int i = 0; i < 3; i++) {
+        for(int iLed = 0; iLed < NUM_LEDS; iLed++) {
+            memset(leds, 0,  NUM_LEDS * sizeof(struct CRGB));
+            switch(i) {
+                case 0: leds[iLed].r = 128; break;
+                case 1: leds[iLed].g = 128; break;
+                case 2: leds[iLed].b = 128; break;
+            }
+            LED.showRGB((unsigned char *)leds, NUM_LEDS);
+        }
+    }
+}   // loop
