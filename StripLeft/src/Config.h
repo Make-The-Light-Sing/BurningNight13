98ebbe707f1b702f554de1e63a8a58bc75057ae8
Jeckel <jeckel@jeckel.fr>
2014-04-04 07:22:59 +0200

    Fast config

--- StripLeft/src/Config.h
+++ StripLeft/src/Config.h
@@ -1,9 +1,21 @@
 #ifndef __CONFIG_H
 #define __CONFIG_H
 
+#include <Color.h>
+
 #define SLAVE_ADDRESS 0x20
 #define NUM_LEDS      180
 #define PIN_STRIP     6
 
+#define NB_SEGMENT  4
+
+CRGB leds[NUM_LEDS];
+
+T_Segment_Config seg_config[NB_SEGMENT] = {
+        { leds,       90},
+        { leds + 10,  90},
+        { leds + 90,  45},
+        { leds + 100, 80}
+    };
 
 #endif
--- StripLeft/src/sketch.ino
+++ StripLeft/src/sketch.ino
@@ -1,10 +1,8 @@
-#include <Color.h>
 #include <FastSPI_LED2.h>
 #include <Segment.h>
 
 #include "Config.h"
 
-CRGB leds[NUM_LEDS];
 TM1809Controller800Mhz<PIN_STRIP> LED;
 SegmentCollection segments;
 
@@ -13,17 +11,11 @@ SegmentCollection segments;
  */
 void setup()
 {
-    T_Segment_Config config;
     LED.init();
 
-    config = { leds, 90 };
-    segments.addSegment(new Segment(config));
-
-    config = { leds + 90, 45 };
-    segments.addSegment(new Segment(config));
-
-    config = { leds + 135, 45 };
-    segments.addSegment(new Segment(config));
+    for(unsigned int i = 0; i < NB_SEGMENT; i++) {
+        segments.addSegment(new Segment(seg_config[i]));
+    }
 }   // setup
 
 /**
